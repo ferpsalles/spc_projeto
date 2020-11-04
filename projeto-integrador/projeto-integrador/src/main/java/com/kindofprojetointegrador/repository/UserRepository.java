a1201b7fd3a1e86ddd7ce826a21c579780d8774e
Fernanda Ramos <55774508+ferpsalles@users.noreply.github.com>
2020-11-03 22:11:25 -0300

    Add files via upload

---
+++ projeto-integrador/projeto-integrador/src/main/java/com/kindofprojetointegrador/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kindofprojetointegrador.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kindofprojeotintegrador.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByUserName(String userName);
+}
